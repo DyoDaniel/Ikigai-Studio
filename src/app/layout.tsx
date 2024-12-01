0d8686c7717b15cd1bab2eaa45c314aff1eea753
Daniel Martini <daniel.danielmartini@hotmail.com>
2024-12-01 14:46:16 -0300

    Improvements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
 import type { Metadata } from "next";
-import {Montserrat} from "next/font/google"
+import {Montserrat, Oswald} from "next/font/google"
 import "./globals.css";
 
 
 const montserrat = Montserrat({
+  weight: ['400','700'],
+  subsets: ['latin']
+})
+
+const oswald = Oswald({
   weight: ['400','700'],
   subsets: ['latin']
 })
@@ -23,7 +28,7 @@ export default function RootLayout({
       <head>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"></link>
       </head>
-      <body className={montserrat.className}>
+      <body className={`${montserrat.className} ${oswald.className}`}>
         {children}
       </body>
     </html>

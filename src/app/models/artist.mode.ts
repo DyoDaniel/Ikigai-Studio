495a699c9403110ef2ccdc6232dec20323b48048
Daniel Martini <daniel.danielmartini@hotmail.com>
2024-12-01 14:14:21 -0300

    add models

---
+++ src/app/models/artist.mode.ts
@@ -0,0 +1,11 @@
+export interface artistModel {
+  color: string;
+  backgroundImage: string;
+  logo: string;
+  instagramUser: string;
+  profileVideo: string;
+  artistName: string;
+  artistSurname: string;
+  instragramUrl: string;
+  artistFooter: string;
+}
